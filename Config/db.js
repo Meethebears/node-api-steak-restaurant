1f2d7b003e061484e32aa9e7c296e64a91d1efc4
Meethebear <thara.poludom@hotmail.com>
2023-11-10 06:53:16 +0700

    fix

--- Config/db.js
+++ Config/db.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.Promise = global.Promise;
-
 const connectDB = async () => {
     await mongoose.connect('mongodb+srv://admin:bank33155@cluster0.iriiwsq.mongodb.net/?retryWrites=true&w=majority')
     .then(() => console.log('connect success'))
