9ab65ba83a6963d06690e43b13204f421d006f91
Meethebear <thara.poludom@hotmail.com>
2023-11-10 06:12:33 +0700

    import file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const morgan = require('morgan')
 const { readdirSync } = require('fs')
 const connectDB = require('./Config/db')
 const products = require('./Routes/product')
-const Product = require('C:/NewProject/server/Models/Products')
+// const Product = require('C:/NewProject/server/Models/Products')
 
 const app = express();
 
@@ -16,9 +16,21 @@ app.use(cors())
 app.use(bodyPaser.json({ limit: '100mb' }))
 app.use(bodyPaser.urlencoded({ extended: true }))
 
+const mongoose = require('mongoose')
+
+const productSchema = mongoose.Schema({
+    name: String,
+    price: {
+       type: Number
+    },
+    img: String
+}, {timestamps: true})
+
+module.exports = mongoose.model('products', productSchema)
+
 app.get('/product',async(req,res) => {
     try {
-        const producted = await Product.find({}).exec();
+        const producted = await productSchema.find({}).exec();
         res.send(producted)
     } catch (err) {
         console.log(err);
@@ -29,5 +41,5 @@ readdirSync('./Routes')
     .map((r) => app.use('/api', require('./Routes/' + r)))
 
 app.listen(5000, () => {
-    console.log("Sever Start");
+    console.log("Server Start");
 })
