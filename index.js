8870edcc5bd0e45ccd8cce5daadde30dac35115f
Meethebear <thara.poludom@hotmail.com>
2023-11-10 06:15:40 +0700

    import file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const morgan = require('morgan')
 const { readdirSync } = require('fs')
 const connectDB = require('./Config/db')
 const products = require('./Routes/product')
-// const Product = require('C:/NewProject/server/Models/Products')
+const Product = require('./Models/Products')
 
 const app = express();
 
@@ -16,27 +16,15 @@ app.use(cors())
 app.use(bodyPaser.json({ limit: '100mb' }))
 app.use(bodyPaser.urlencoded({ extended: true }))
 
-const mongoose = require('mongoose')
-
-const productSchema = mongoose.Schema({
-    name: String,
-    price: {
-       type: Number
-    },
-    img: String
-}, {timestamps: true})
-
-module.exports = mongoose.model('products', productSchema)
-
-app.get('/product',async(req,res) => {
-    try {
-        const producted = await productSchema.find({}).exec();
-        res.send(producted)
-    } catch (err) {
-        console.log(err);
-        res.status(500).send('Server Error')
-    }
-})
+// app.get('/product',async(req,res) => {
+//     try {
+//         const producted = await Product.find({}).exec();
+//         res.send(producted)
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).send('Server Error')
+//     }
+// })
 readdirSync('./Routes')
     .map((r) => app.use('/api', require('./Routes/' + r)))
 
