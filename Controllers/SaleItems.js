18afac30cb689a51ede73fe00ce51cb445e852b3
Meethebears <thara.poludom@hotmail.com>
2024-03-28 20:00:23 +0700

    add api

--- Controllers/SaleItems.js
+++ Controllers/SaleItems.js
@@ -6,7 +6,8 @@ exports.read = async (req, res) => {
         const id = req.params.id
         const saleItem = await SaleItems.find({ _id: id }).exec();
         res.send(saleItem)
-    } catch (err) {
+    }
+    catch (err) {
         console.log(err);
         res.status(500).send('Sever Error')
     }
@@ -25,14 +26,15 @@ exports.create = async (req, res) => {
         let order = req.body.order
         let data = {
             order,
-            tablenumber : req.body.tablenumber,
-            totalprice : total,
+            tablenumber: req.body.tablenumber,
+            totalprice: total,
             payment: 'not paid'
-          }
+        }
         const saleItem = await SaleItems(data).save()
         res.send(saleItem)
 
-    } catch (err) {
+    }
+    catch (err) {
         console.log(err);
         res.status(500).send('Server Error')
     }
@@ -42,7 +44,8 @@ exports.list = async (req, res) => {
     try {
         const SaleItem = await SaleItems.find({}).exec();
         res.send(SaleItem)
-    } catch (err) {
+    }
+    catch (err) {
         console.log(err);
         res.status(500).send('Server Error')
     }
@@ -53,8 +56,67 @@ exports.update = async (req, res) => {
         const id = req.params.id
         const updated = await SaleItems.findOneAndUpdate({ _id: id }, req.body, { new: true }).exec()
         res.send(updated)
-    } catch (err) {
+    }
+    catch (err) {
         console.log(err);
         res.status(500).send('Server Error')
+    }
+}
+
+exports.listTotal = async (req, res) => {
+    try {
+        let Total = []
+        const TotalSale = await SaleItems.find({ "payment": "paid" }, { "totalprice": 1, _id: 0 })
+        TotalSale.map(item => Total.push(item.totalprice))
+        let sum_total = Total.reduce((prev, curr) => prev + curr, 0)
+        res.send([sum_total])
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).send("Server Error")
+    }
+}
+
+exports.listTodaySales = async (req, res) => {
+    try {
+        let total_today = []
+        let start = new Date()
+        start.setHours(7, 0, 0, 0)
+        let startISOdate = start.toISOString()
+        let end = new Date()
+        end.setHours(30, 59, 59, 999)
+        let endISOdate = end.toISOString()
+        const TodaySales = await SaleItems.find({ updated_at: { $gte: startISOdate, $lt: endISOdate } })
+        TodaySales.map(item => total_today.push(item.totalprice))
+        let sum_total_today = total_today.reduce((prev, curr) => prev + curr, 0)
+        res.send([sum_total_today])
+
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).send("Server Error")
+    }
+}
+
+exports.listMonthSales = async (req, res) => {
+    try {
+        const total_month = []
+        const month = new Date().getMonth()+1;
+        const year = new Date().getFullYear();
+        let start = new Date(year+"-"+month+"-"+"1");
+        start.setHours(7, 0, 0, 0);
+        let startISOdate = start.toISOString();
+        let end = new Date(year+"-"+month+"-"+"31");
+        end.setHours(30, 59, 59, 999);
+        let endISOdate = end.toISOString();
+        const MonthSales = await SaleItems.find({ updated_at: { $gte: startISOdate, $lt: endISOdate }, payment: "paid" })
+        MonthSales.map(item => total_month.push(item.totalprice))
+        let sum_total_month = total_month.reduce((prev, curr) => prev + curr, 0)
+        res.send([sum_total_month]);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).sent("Server Error")
+
     }
 }
--- Routes/sale_items.js
+++ Routes/sale_items.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const { create, list, read, update } = require('../Controllers/SaleItems')
+const { create, list, read, update, listTotal, listTodaySales, listMonthSales, listTotalMonthSales } = require('../Controllers/SaleItems')
 
 router.post('/sale_items', create)
 router.get('/sale_items', list)
 router.get('/sale_items/:id', read)
 router.put('/sale_items/:id', update)
-
+router.get('/sale_item/total-sales',listTotal)
+router.get('/sale_item/today-sales',listTodaySales)
+router.get('/sale_item/month-sales',listMonthSales)
 
 module.exports = router
